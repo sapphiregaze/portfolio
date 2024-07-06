721e43b21e7601387b4c177d8c72e41cf7c3955d
SapphireGaze <jasminehuang2048@outlook.com>
2024-07-06 03:42:00 -0700

    Refactored Main & Navbar Into Layout:  - Refactored main component and navbar component into global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Zilla_Slab } from "next/font/google";
 
+import { Box } from "@mui/material";
+
 import "./globals.css";
-import { ThemeProviderWrapper } from "../components/ThemeProviderWrapper";
+import { Navbar } from "@/components/Navbar";
+import { ThemeProviderWrapper } from "@/components/ThemeProviderWrapper";
 
 const zilla = Zilla_Slab({ weight: "400", subsets: ["latin"] });
 
@@ -19,7 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={zilla.className}>
-        <ThemeProviderWrapper>{children}</ThemeProviderWrapper>
+        <ThemeProviderWrapper>
+          <Box component="main" sx={{ padding: 5 }}>
+            <Navbar />
+            {children}
+          </Box>
+        </ThemeProviderWrapper>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Box, Toolbar, Typography } from "@mui/material";
-import { Navbar } from "@/components/Navbar";
-
 export default function Home() {
-  return (
-    <Box component="main" sx={{ padding: 5 }}>
-      <Navbar />
-    </Box>
-  );
+  return <></>;
 }
